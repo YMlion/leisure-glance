53f27f52c094a8089df2470e71ecd1ba9039b3b3
YMlion <lm_yao@163.com>
2017-07-07 17:47:24 +0800

    modify construction

--- app/src/main/java/com/ymlion/leisure/module/video/VideoAdapter.kt
+++ app/src/main/java/com/ymlion/leisure/module/video/VideoAdapter.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.ymlion.lib.base.RvBaseAdapter
 import com.ymlion.lib.base.ViewHolder
 
-class VideoAdapter(context: Context?, list: MutableList<String>?, layoutRes: Int) : RvBaseAdapter<String>(context, list, layoutRes) {
+class VideoAdapter(context: Context?, list: MutableList<String>?, layoutRes: Int) : RvBaseAdapter<String>(list, layoutRes) {
     override fun onBind(holder: ViewHolder?, model: String?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
